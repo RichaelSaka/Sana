5ff3f44b3f594b30a240adb24f0cf862ae92fca4
ALINA YU <alinayu@college.harvard.edu>
2024-09-29 00:37:37 -0400

    Initial Commit

---
+++ Sana/SanaApp.swift
@@ -0,0 +1,17 @@
+//
+//  SanaApp.swift
+//  Sana
+//
+//  Created by Alina Yu on 9/29/24.
+//
+
+import SwiftUI
+
+@main
+struct SanaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
